0fc3ccbfb40d11ffb6f6782872c9e72f6810985d
Dawn-deer <56643760+Dawn-deer@users.noreply.github.com>
2020-11-19 19:28:45 +0800

    Update ImageSoures.java

--- app/src/main/java/com/example/puzzlegame/module/ImageSoures.java
+++ app/src/main/java/com/example/puzzlegame/module/ImageSoures.java
@@ -3,9 +3,7 @@ package com.example.puzzlegame.module;
 
 import com.example.puzzlegame.R;
 
-/**
- * Created by Axe on 2017/7/15.
- */
+
 
 public class ImageSoures {
     public static final int[] imageSours = {R.mipmap.dmxyzy, R.mipmap.dfzs, R.mipmap.lb, R.mipmap.ll,R.mipmap.w,R.mipmap.sdhy};
